f18a7dfecea7ad1476d261a9f142eca6b3e2c6b5
yanganto <yanganto@gmail.com>
2020-05-14 14:13:46 +0800

    Show plots of variable versus submit rounds (#2)
    - show the challenge time vs submit rounds plot
    - show the bonds vs submit rounds plot
    - let plot as a feature, and default enabled

--- src/error.rs
+++ src/error.rs
@@ -12,6 +12,9 @@ pub enum Error {
     CliError(String),
     #[fail(display = "Patch Parameter `{}` is not valid", 0)]
     PatchParameterError(String),
+    #[cfg(feature = "plot")]
+    #[fail(display = "Plotting Error: {}", 0)]
+    PlotError(String),
     #[fail(display = "Unexpected: {}", 0)]
     UnknownError(&'static str),
 }
